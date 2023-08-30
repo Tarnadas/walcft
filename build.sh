d2afb3d1c5ef753606e7b44e18c63fd9f006dbcb
Mario Reder <mario.reder@pm.me>
2023-08-30 08:18:12 +0200

    reproducible builds

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd "`dirname $0`"
-
-mkdir -p out
-cargo build --all --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/*.wasm out/contract.wasm
