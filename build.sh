51c43c795c5f6a2ef1a7fd0306693bed920a01a5
Mario Reder <mario.reder@pm.me>
2023-08-30 06:59:19 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 cd "`dirname $0`"
+
+mkdir -p out
 cargo build --all --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm out/contract.wasm
